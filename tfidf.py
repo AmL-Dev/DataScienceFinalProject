e4effb8f83c62ebccb44016b43926dd0124de9e3
Sara Heiberg <sarabhe@stud.ntnu.no>
2022-11-29 17:57:31 +0100

    TFIDF comments updated

--- tfidf.py
+++ tfidf.py
@@ -1,44 +1,44 @@
- 
+
+# Importing libraries
 from mrjob.job import MRJob
 from mrjob.step import MRStep
-import pandas as pd #using pandas to find the number of books in the corpus
-import spacy #using the spacy library to remove stop words 
-from nltk.stem import PorterStemmer #reduces the word to the root node
-from nltk.stem import SnowballStemmer #using snowball to reduce the word to the root node 
+import pandas as pd 
+import spacy  
+from nltk.stem import PorterStemmer 
+from nltk.stem import SnowballStemmer 
 import string 
 import math
 import re 
 
-#finding the total number of books in the corpus (needed for the tfidf calculations)
+# Finding the total number of books in the corpus (needed for the tfidf calculations)
 test = pd.read_csv('data/books_metadata.csv')
 M = len(test)
 
-#used to filter out stopwords from the description (english language small model of spacy)
+# Using the spacy library to load the english stopwords 
 en = spacy.load('en_core_web_sm')
 stopwords = en.Defaults.stop_words
 
-#string of special characters, used to remove special characters from the description
+# Using the punctuation attribute from the string library to store a string with all the special characters 
 special_char = r'[' + string.punctuation + ']'
 
-#standardize words that share the same suffix and that normally are derivations of gramatically similar words
-#two different stemmers, the snowball is more agressive
+# Using the stemmer to reduce the words to a common base form, by removing the affix
+# Two different stemmers, the snowball is more agressive
 porter = PorterStemmer()
 snowball = SnowballStemmer(language='english')
 
-#used to search for groups that have alphanumerics or ' that are 1 or longer. I.e. breaks a line into words.
+# Using regex to search for groups that have alphanumerics or ', i.e. to break a line into words
 WORD_RE = re.compile(r"[\w']+") 
 
-#only yield the words with a tfidf score above the threshold, in order to reduce the number of computations
-threshold = 0
+# To increase computation efficiency, MapReduce should only yield words with tfidf score above a specific threshold 
+# An appropriate threshold is found calculationg the 5th procentile of the tfidf scores 
+threshold = 0.0219
 
 
-class MR_TFIDF(MRJob): #class that inherits from MRJob 
+class MR_TFIDF(MRJob):  
 
-
+    # 4 MapReduce steps 
     def steps(self):
-        #need to define the order of mappers and reducers, so that the functions takes in the right key and value
-        #only one mapper and reducer per step, so therefore need several steps
-        #do we need the combiner between the mapper and reducer??
+        
         return [ 
             MRStep(mapper=self.mapper_1, reducer=self.reducer_1),
             MRStep(mapper=self.mapper_2, reducer = self.reducer_2),
@@ -46,23 +46,24 @@ class MR_TFIDF(MRJob): #class that inherits from MRJob
             MRStep(mapper = self.mapper_4)
         ]
 
-    def split_and_clean(self, line):  #takes in a key and a value, here the key is ignored
+    # Takes a description as input, removes stopwords, punctuation and convert to lowercase
+    # Returns a list of words.
+    def split_and_clean(self, line):  
         words = []
         line = re.sub(special_char, ' ', line) #remove special characters from line
-        for word in WORD_RE.findall(line): #for each word in that line
-            if word.lower() not in stopwords and len(word)>1: #if the word is not a stop word 
-                words.append(snowball.stem(word.lower())) #append the root of the word
-        return words #return the words
+        for word in WORD_RE.findall(line): #for each word in the line
+            if word.lower() not in stopwords and len(word)>1: #if the word is not a stop word and length > 1
+                words.append(snowball.stem(word.lower())) #append the root of the word to a list
+        return words #return the list of words
 
-    #key - ignored
-    #values - corpus of documents (i.e. the data frame books_metadata)
-    def mapper_1(self, _, line):  #each line is a row, i.e. a book 
-        idFound = re.search(r"(^\d+),", line)
-        descriptionFound = re.search(r",\"(.+)\",", line)
-        if idFound: 
-            id = int(idFound.group(1))
-            if descriptionFound:
-                d = descriptionFound.group(1)
+    # Mapper of the first MapReduce job
+    def mapper_1(self, _, line):  
+        id_found = re.search(r"(^\d+),", line)
+        description_found = re.search(r",\"(.+)\",", line)
+        if id_found: 
+            id = int(id_found.group(1))
+            if description_found:
+                d = description_found.group(1)
                 description = str()
                 flag = True
                 for char in d:
@@ -71,22 +72,19 @@ class MR_TFIDF(MRJob): #class that inherits from MRJob
                     if flag:
                         description += char
                 for word in self.split_and_clean(description): 
-                    yield (word,id), 1   # returns key-value pairs ((word, id), 1)
+                    yield (word,id), 1  
 
 
-    #key - the key which was yielded by the mapper, i.e. (word, id). (key[0] = word, key[1]=id)
-    #values - A generator which yields all values yielded by the mapper which correspond to key, i.e. a list of 1's
+    # Reducer of the first MapReduce job
     def reducer_1(self, word_id, counts): 
-        yield (word_id[0], word_id[1]), sum(counts) # returns key-value pairs ((word, id), n)
+        yield (word_id[0], word_id[1]), sum(counts) 
 
-    #key - (word, id)
-    #values - n
+    # Mapper of the second MapReduce job
     def mapper_2(self, word_id, n):
         word, id = word_id[0], word_id[1], 
-        yield id, (word,n) # returns key-value pairs (id, (word,n))
+        yield id, (word,n) 
 
-    #key - id
-    #values - list of (word,n) pairs 
+    # Reducer of the second MapReduce job 
     def reducer_2(self, id, word_n):
         
         word_list = []
@@ -104,18 +102,16 @@ class MR_TFIDF(MRJob): #class that inherits from MRJob
             num_el+=1
 
         for i in range(num_el):
-            yield (word_list[i],id), (n_list[i],N)    #returns key-value pairs ((word,id), (n,N))
+            yield (word_list[i],id), (n_list[i],N)   
 
-    #key - (word, id)
-    #values - (n, N) (only one pair)
+    # Mapper of the third MapReduce job 
     def mapper_3(self, word_id, n_N):
         word, id = word_id[0], word_id[1]
         n, N = n_N[0], n_N[1]
         yield word, (id, n, N, 1)
 
 
-    #key - word
-    #values - list of key-value (id,n,N,1) pairs
+    # Reducer of the third MapReduce job
     def reducer_3(self, word, id_n_N_counts):
 
         id_list = []
@@ -139,6 +135,7 @@ class MR_TFIDF(MRJob): #class that inherits from MRJob
         for i in range(num_el):
             yield (word, id_list[i]), (n_list[i], N_list[i],m)
 
+    # Mapper of the fourth MapReduce job 
     def mapper_4(self, word_id, n_N_m):
         word, id = word_id[0], word_id[1]
         n, N, m = n_N_m[0], n_N_m[1], n_N_m[2]
